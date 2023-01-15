9568abac8fe3dda54ce414ac08d0557b1ef989d9
buncram <cramium@kosagi.com>
2023-01-16 02:50:17 +0800

    add coreuser setup to the console demo process

--- services/console/src/main.rs
+++ services/console/src/main.rs
@@ -11,8 +11,41 @@ fn main() {
     )
     .expect("couldn't map Core Control CSR range");
     let mut core_csr = CSR::new(csr.as_mut_ptr() as *mut u32);
-
     core_csr.wfo(utra::main::REPORT_REPORT, 0x600d_0000);
+
+    core_csr.wfo(utra::main::REPORT_REPORT, 0xa51d_0000);
+    let coreuser_csr = xous::syscall::map_memory(
+        xous::MemoryAddress::new(utra::coreuser::HW_COREUSER_BASE),
+        None,
+        4096,
+        xous::MemoryFlags::R | xous::MemoryFlags::W,
+    )
+    .expect("couldn't map Core User CSR range");
+    let mut coreuser = CSR::new(coreuser_csr.as_mut_ptr() as *mut u32);
+    // first, clear the ASID table to 0
+    for asid in 0..512 {
+        coreuser.wo(utra::coreuser::SET_ASID,
+            coreuser.ms(utra::coreuser::SET_ASID_ASID, asid)
+            | coreuser.ms(utra::coreuser::SET_ASID_TRUSTED, 0)
+        );
+    }
+    // set my PID to trusted
+    coreuser.wo(utra::coreuser::SET_ASID,
+        coreuser.ms(utra::coreuser::SET_ASID_ASID, xous::process::id() as u32)
+        | coreuser.ms(utra::coreuser::SET_ASID_TRUSTED, 1)
+    );
+    // set the required `mpp` state to user code (mpp == 0)
+    coreuser.wfo(utra::coreuser::SET_PRIVILEGE_MPP, 0);
+    // turn on the coreuser computation
+    coreuser.wo(utra::coreuser::CONTROL,
+        coreuser.ms(utra::coreuser::CONTROL_ASID, 1)
+        | coreuser.ms(utra::coreuser::CONTROL_ENABLE, 1)
+        | coreuser.ms(utra::coreuser::CONTROL_PRIVILEGE, 1)
+    );
+    // turn off coreuser control updates
+    coreuser.wo(utra::coreuser::PROTECT, 1);
+    core_csr.wfo(utra::main::REPORT_REPORT, 0xa51d_600d);
+
     log::info!("my PID is {}", xous::process::id());
     let tt = xous_api_ticktimer::Ticktimer::new().unwrap();
     let mut total = 0;
@@ -23,11 +56,11 @@ fn main() {
         let now = tt.elapsed_ms();
         core_csr.wfo(utra::main::REPORT_REPORT, 0x2222_0000 + iter);
         total += now;
-        if now > 0x20 && now < 0x30 {
+        if iter > 0x20 && iter < 0x30 {
             tt.sleep_ms(1).unwrap();
-        } else if now < 0x40 {
+        } else if iter < 0x40 {
             tt.sleep_ms(1).ok();
-        } else if now > 0x50 {
+        } else if iter > 0x50 {
             break;
         }
         // something lame to just conjure a memory message
